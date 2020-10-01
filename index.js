cba15dffc9ad59b8fd766f692d780a1cf88cc66d
Eric Lich <erick_lich@hotmail.com>
2020-10-01 11:44:36 -0300

    add slide sidebar functionality

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const hideAside = () => {
         let aside = document.querySelector(".aside");
+        let relleno = document.querySelector(".relleno");
     
-        if(aside.style.display == "none"){
-            aside.style.display = "block";
+        if(aside.style.width == "0px"){
+            aside.style.width = "250px";
+            relleno.style.width = "250px";
         }else{
-            aside.style.display = "none";
+            aside.style.width = "0px";
+            relleno.style.width = "0px";
         }
 
 }
